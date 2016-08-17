1745c7d6eaf0c07e987acdb5ecfd673fe77cebe9
Evan Richardson <richardsone@msoe.edu>
2016-08-17 00:07:07 -0500

    Testing

--- tasks/register/buildProd.js
+++ tasks/register/buildProd.js
@@ -19,7 +19,6 @@ module.exports = function(grunt) {
   grunt.registerTask('buildProd', [
     'compileAssets',
     'concat',
-    'uglify',
     'cssmin',
     'linkAssetsBuildProd',
     'clean:build',
--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -15,7 +15,6 @@ module.exports = function(grunt) {
   grunt.registerTask('prod', [
     'compileAssets',
     'concat',
-    'uglify',
     'cssmin',
     'sails-linker:prodJs',
     'sails-linker:prodStyles',
