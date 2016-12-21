c1678887f2e2688e8d4f93776a0ed7821ed404d4
Andy Pethan <andy.pethan@byron.k12.mn.us>
2016-12-21 22:06:43 +0000

    lost in a daze

--- api/services/RespService.js
+++ api/services/RespService.js
@@ -8,6 +8,18 @@ module.exports = {
   // response for successes
   s: function (res, data) {
     return res.json({ status: 'success', data: data});
-  }
+  },
+  
+  // helper for generic callbacks
+  cb: function (options, name, return_val) {
+    if ('callback' in options) {
+      options[name] = return_val;
+      callback_function = options.callback;
+      delete options.callback;
+      callback_function(options);
+    } else {
+      return RespService.s(options.res, return_val);
+    }
+  },
 
 }
